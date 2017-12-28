4dd9c601ca2f0b19e53bf09e6352cd7e9be3af1f
liucan891209 <liucan891209@gmail.com>
2017-12-28 15:38:28 +0100

    l'ajout du trigger pour lambda

--- infra-BBL/main.tf
+++ infra-BBL/main.tf
@@ -1,55 +1,62 @@
 resource "aws_elasticache_replication_group" "redis-instance" {
-  replication_group_id          = "redis"
+  replication_group_id = "redis"
   replication_group_description = "Redis cluster for Hashicorp ElastiCache example"
-  engine               = "redis"
-  number_cache_clusters         = 2
+  engine = "redis"
+  number_cache_clusters = 2
 
-  node_type            = "cache.t2.micro"
-  port                 = 6379
+  node_type = "cache.t2.micro"
+  port = 6379
   parameter_group_name = "default.redis3.2"
 
   snapshot_retention_limit = 0
 
 
-  security_group_ids = ["sg-ac2acbc4"]
+  security_group_ids = [
+    "sg-ac2acbc4"]
 }
 
 
 resource "aws_db_instance" "pg-vote" {
-  identifier           = "pg-vote"
-  allocated_storage    = 20
-  storage_type         = "gp2"
-  engine               = "postgres"
-  engine_version       = "9.6.3"
-  instance_class       = "db.t2.micro"
-  name                 = "postgres"
-  username             = "postgres"
-  password             = "postgres"
-  vpc_security_group_ids = ["sg-ac2acbc4"]
+  identifier = "pg-vote"
+  allocated_storage = 20
+  storage_type = "gp2"
+  engine = "postgres"
+  engine_version = "9.6.3"
+  instance_class = "db.t2.micro"
+  name = "postgres"
+  username = "postgres"
+  password = "postgres"
+  vpc_security_group_ids = [
+    "sg-ac2acbc4"]
 }
 
 data "aws_ami" "ec2-linux" {
   most_recent = true
   filter {
     name = "name"
-    values = ["amzn-ami-*-x86_64-gp2"]
+    values = [
+      "amzn-ami-*-x86_64-gp2"]
   }
   filter {
     name = "virtualization-type"
-    values = ["hvm"]
+    values = [
+      "hvm"]
   }
   filter {
     name = "owner-alias"
-    values = ["amazon"]
+    values = [
+      "amazon"]
   }
 }
 
 resource "aws_instance" "web-result" {
-  ami           = "${data.aws_ami.ec2-linux.id}"
+  ami = "${data.aws_ami.ec2-linux.id}"
   instance_type = "t2.micro"
 
   subnet_id = "subnet-f578e98e"
-  vpc_security_group_ids = ["sg-844242ec", "sg-ac2acbc4"]
+  vpc_security_group_ids = [
+    "sg-844242ec",
+    "sg-ac2acbc4"]
 
   tags {
     Name = "catvsdog-result"
@@ -57,10 +64,12 @@ resource "aws_instance" "web-result" {
 }
 
 resource "aws_instance" "web-vote" {
-  ami           = "${data.aws_ami.ec2-linux.id}"
+  ami = "${data.aws_ami.ec2-linux.id}"
   instance_type = "t2.micro"
   subnet_id = "subnet-f578e98e"
-  vpc_security_group_ids = ["sg-09a08a61", "sg-ac2acbc4"]
+  vpc_security_group_ids = [
+    "sg-09a08a61",
+    "sg-ac2acbc4"]
 
   tags {
     Name = "catvsdog-vote"
@@ -88,14 +97,35 @@ resource "aws_iam_role" "iam_for_lambda" {
 EOF
 }
 
+resource "aws_cloudwatch_event_rule" "trigger-rule" {
+  name = "worker-trigger"
+  description = "trigger db one minute"
+
+  schedule_expression = "rate(1 minute)"
+}
+
+resource "aws_cloudwatch_event_target" "trigger-target" {
+  rule = "${aws_cloudwatch_event_rule.trigger-rule.name}"
+  target_id = "SendToVoteWorker"
+  arn = "${aws_lambda_function.vote-worker.arn}"
+}
+
+resource "aws_lambda_permission" "worker-permission" {
+  statement_id = "worker-permission"
+  action = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.vote-worker.function_name}"
+  principal = "events.amazonaws.com"
+  source_arn = "${aws_cloudwatch_event_rule.trigger-rule.arn}"
+}
+
 resource "aws_lambda_function" "vote-worker" {
-  function_name    = "voteWorker"
-  s3_bucket        = "archi-terraform-state"
-  s3_key           = "catvsdog-worker.zip"
+  function_name = "voteWorker"
+  s3_bucket = "archi-terraform-state"
+  s3_key = "catvsdog-worker.zip"
   s3_object_version = "EddNNvIlRGC9igIiQZSKALV2bbWEt56U"
-  role             = "${aws_iam_role.iam_for_lambda.arn}"
-  handler          = "app.voteWorker"
-  runtime          = "python2.7"
+  role = "${aws_iam_role.iam_for_lambda.arn}"
+  handler = "app.voteWorker"
+  runtime = "python2.7"
 
   environment {
     variables = {
